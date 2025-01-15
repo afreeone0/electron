03e32431a0948dbff982b86d0386bf852be6416c
afreeone <7lmarklibriets@gmail.com>
2025-01-15 16:31:39 +0300

    added forms for login, regstrtn, and profile

--- electron/users/forms.py
+++ electron/users/forms.py
@@ -1,1 +1,27 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
+from .models import User
+from django import forms
+
+
+class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('nickname', 'username', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+    nickname = forms.CharField(widget=forms.TextInput(attrs={}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'readonly': True}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'readonly': True}))
+
+    class Meta:
+        model = User
+        fields = ('nickname', 'username', 'email')
--- electron/users/views.py
+++ electron/users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .forms import UserLoginForm, UserProfileForm, UserRegistrationForm
+from django.contrib import auth, messages
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.username
+            password = form.password
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                url = reverse('index')
+                redirect(url, permanent=True)
+    else:
+        form = UserLoginForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/login.html', context=context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse('profile')
+            return redirect(url, permanent=True)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/profile.html', context=context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registered successfully!')
+            url = reverse('login')
+            return redirect(url, permanent=True)
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/registration.html', context=context)
