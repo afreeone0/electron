4128a358d42f5bfdeb80fca20887ca8bef92d03f
afreeone <7lmarklibriets@gmail.com>
2025-01-15 15:50:40 +0300

    added user, product, category and cart models

--- electron/store/models.py
+++ electron/store/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    category_slug = models.SlugField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    # image = models.ImageField()
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(to=Category, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=13, decimal_places=2)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} | {self.price} | {self.category}"
+
+
+class Cart(models.Model):
+
+    class Meta:
+        verbose_name = 'cart'
+        verbose_name_plural = 'carts'
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
