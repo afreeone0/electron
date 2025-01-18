96aa4f6521e713c2df7627ba201aa7bc227fff69
afreeone <7lmarklibriets@gmail.com>
2025-01-18 20:11:55 +0300

    WARNING: DON'T USE ANYTHING EARLIER THIS COMMIT BLYATgit status!

--- electron/electron/settings.py
+++ electron/electron/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-97c66wfp(mp1cg@7qqkk!7v2!-y5yo0uy=r=^s9q!i2fk_1q9)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -81,7 +83,7 @@ WSGI_APPLICATION = 'electron.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / os.environ.get('DATABASE_NAME'),
     }
 }
 
