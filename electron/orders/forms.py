009a0c80a66065df44889389ecf066a724cdbc05
afreeone <7lmarklibriets@gmail.com>
2025-01-31 19:49:56 +0300

    phone number validation was edited

--- electron/orders/forms.py
+++ electron/orders/forms.py
@@ -21,16 +21,19 @@ class CreateOrderForm(forms.Form):
     )
 
     def clean_phone_number(self):
-        data = self.cleaned_data['phone_number']
-
-        if not data.isdigit():
-            raise forms.ValidationError("Номер телефона должен содержать только цифры")
-
-        pattern = re.compile(r'^\d{10}$')
-        if not pattern.match(data):
+        phone_number = self.cleaned_data['phone_number']
+        if len(phone_number) > 25:
             raise forms.ValidationError("Неверный формат номера")
 
-        return data
+        for x in '() +-':
+            phone_number = phone_number.replace(x, '')
+
+        regex = r'^\d{11}$'
+        pattern = re.compile(regex)
+        if not pattern.match(phone_number):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return phone_number
 
     # first_name = forms.CharField(
     #     widget=forms.TextInput(
